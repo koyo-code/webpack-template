4cf71da046060665b2fd7d5161800455fb0b66e4
koyo code <87898051+koyo-code@users.noreply.github.com>
2023-01-07 09:42:33 +0900

    コミット

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,33 +1,166 @@
-import "./shader/vertex";
-import "./shader/fragment";
+import vertexShader from "./shader/vertex";
+import fragmentShader from "./shader/fragment";
 
 import * as THREE from "three";
 
+const canvasEl = document.getElementById("webgl-canvas");
+const canvasSize = {
+  w: window.innerWidth,
+  h: window.innerHeight,
+};
+
+const renderer = new THREE.WebGLRenderer({ canvas: canvasEl });
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(canvasSize.w, canvasSize.h);
+
+// ウィンドウとwebGLの座標を一致させるため、描画がウィンドウぴったりになるようカメラを調整
+const fov = 60; // 視野角
+const fovRad = (fov / 2) * (Math.PI / 180);
+const dist = canvasSize.h / 2 / Math.tan(fovRad);
+const camera = new THREE.PerspectiveCamera(fov, canvasSize.w / canvasSize.h, 0.1, 2000);
+camera.position.z = dist;
+
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+const loader = new THREE.TextureLoader();
 
-// const geometry = new THREE.BoxGeometry();
-// const geometry = new THREE.PlaneGeometry(10, 10);
-// const geometry = new THREE.SphereGeometry();
-const geometry = new THREE.TorusGeometry(5, 1, 200, 20);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// 画像をテクスチャにしたplaneを扱うクラス
+class ImagePlane {
+  constructor(mesh, img) {
+    this.refImage = img;
+    this.mesh = mesh;
+  }
 
-camera.position.z = 50;
+  setParams() {
+    // 参照するimg要素から大きさ、位置を取得してセット
+    const rect = this.refImage.getBoundingClientRect();
 
-let i = 0;
-function animate() {
-  requestAnimationFrame(animate);
-  // console.log(i++);
-  cube.rotation.x = cube.rotation.x + 0.01;
-  cube.rotation.y += 0.01;
+    this.mesh.scale.x = rect.width;
+    this.mesh.scale.y = rect.height;
 
-  renderer.render(scene, camera);
+    const x = rect.left - canvasSize.w / 2 + rect.width / 2;
+    const y = -rect.top + canvasSize.h / 2 - rect.height / 2;
+    this.mesh.position.set(x, y, this.mesh.position.z);
+  }
+
+  update(offset) {
+    this.setParams();
+
+    this.mesh.material.uniforms.uTime.value = offset;
+  }
 }
 
-animate();
+// Planeメッシュを作る関数
+const createMesh = (img) => {
+  const texture = loader.load(img.src);
+
+  const uniforms = {
+    uTexture: { value: texture },
+    uImageAspect: { value: img.naturalWidth / img.naturalHeight },
+    uPlaneAspect: { value: img.clientWidth / img.clientHeight },
+    uTime: { value: 0 },
+  };
+  const geo = new THREE.PlaneBufferGeometry(1, 1, 100, 100); // 後から画像のサイズにscaleするので1にしておく
+  const mat = new THREE.ShaderMaterial({
+    uniforms,
+    vertexShader,
+    fragmentShader,
+  });
+
+  const mesh = new THREE.Mesh(geo, mat);
+
+  return mesh;
+};
+
+// スクロール追従
+let targetScrollY = 0; // スクロール位置
+let currentScrollY = 0; // 線形補間を適用した現在のスクロール位置
+let scrollOffset = 0; // 上記2つの差分
+
+// 開始と終了をなめらかに補間する関数
+const lerp = (start, end, multiplier) => {
+  return (1 - multiplier) * start + multiplier * end;
+};
+
+const updateScroll = () => {
+  // スクロール位置を取得
+  targetScrollY = document.documentElement.scrollTop;
+  // リープ関数でスクロール位置をなめらかに追従
+  currentScrollY = lerp(currentScrollY, targetScrollY, 0.1);
+
+  scrollOffset = targetScrollY - currentScrollY;
+};
+
+// 慣性スクロール
+const scrollArea = document.querySelector(".scrollable");
+const setupScrollHeight = () => {
+  document.body.style.height = `${scrollArea.getBoundingClientRect().height}px`;
+};
+
+setupScrollHeight();
+
+const imagePlaneArray = [];
+
+// 毎フレーム呼び出す
+const loop = () => {
+  updateScroll();
+
+  scrollArea.style.transform = `translate3d(0,${-currentScrollY}px,0)`;
+  for (const plane of imagePlaneArray) {
+    plane.update(scrollOffset);
+  }
+  renderer.render(scene, camera);
+
+  requestAnimationFrame(loop);
+};
+
+// リサイズ処理
+let timeoutId = 0;
+const resize = () => {
+  // three.jsのリサイズ
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  canvasSize.w = width;
+  canvasSize.h = height;
+
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(width, height);
+
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+
+  // カメラの距離を計算し直す
+  const fov = 60;
+  const fovRad = (fov / 2) * (Math.PI / 180);
+  const dist = canvasSize.h / 2 / Math.tan(fovRad);
+  camera.position.z = dist;
+
+  // スクロールコンテナの高さを取り直す
+  setupScrollHeight();
+};
+
+const main = () => {
+  window.addEventListener("load", () => {
+    const imageArray = [...document.querySelectorAll("img")];
+    for (const img of imageArray) {
+      const mesh = createMesh(img);
+      scene.add(mesh);
+
+      const imagePlane = new ImagePlane(mesh, img);
+      imagePlane.setParams();
+
+      imagePlaneArray.push(imagePlane);
+    }
+    loop();
+  });
+
+  // リサイズ（負荷軽減のためリサイズが完了してから発火する）
+  window.addEventListener("resize", () => {
+    if (timeoutId) clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(resize, 200);
+  });
+};
+
+main();
--- src/scripts/shader/fragment.glsl
+++ src/scripts/shader/fragment.glsl
@@ -1,37 +1,27 @@
-precision mediump float;
+      varying vec2 vUv;
+      uniform sampler2D uTexture;
+      uniform float uImageAspect;
+      uniform float uPlaneAspect;
+      uniform float uTime;
 
-#pragma glslify: noise2 = require(glsl-noise/simplex/2d);
-#pragma glslify: noise3 = require(glsl-noise/simplex/3d);
+      void main(){
+        // 画像のアスペクトとプレーンオブジェクトのアスペクトを比較し、短い方に合わせる
+        vec2 ratio = vec2(
+          min(uPlaneAspect / uImageAspect, 1.0),
+          min((1.0 / uPlaneAspect) / (1.0 / uImageAspect), 1.0)
+        );
 
-varying vec2 vUv;
-uniform sampler2D uTexCurrent;
-uniform sampler2D uTexNext;
-uniform sampler2D uTexDisp;
-uniform float uTick;
-uniform float uProgress;
-uniform vec2 uNoiseScale;
-float parabola( float x, float k ) {
-  return pow( 4. * x * ( 1. - x ), k );
-}
-void main() {
+        // 計算結果を用いてテクスチャを中央に配置
+        vec2 fixedUv = vec2(
+          (vUv.x - 0.5) * ratio.x + 0.5,
+          (vUv.y - 0.5) * ratio.y + 0.5
+        );
 
-  // // n => -1 ~ 1
-  // float n = noise2(vec2(vUv.x * uNoiseScale.x, vUv.y * uNoiseScale.y));
-  // // n => -1 ~ 0
-  // n = n * 0.5 - 0.5;
-  // // uProgress => 0 ~ 1
-  // n = n + uProgress;
+        vec2 offset = vec2(0.0, uTime * 0.0005);
+        float r = texture2D(uTexture, fixedUv + offset).r;
+        float g = texture2D(uTexture, fixedUv + offset * 0.5).g;
+        float b = texture2D(uTexture, fixedUv).b;
+        vec3 texture = vec3(r, g, b);
 
-  // n = step(0.0, n);
-
-  vec4 texDisp = texture(uTexDisp, vUv);
-  float disp = texDisp.r;
-  disp = disp * parabola(uProgress, 1.0);
-  vec2 dispUv = vec2(vUv.x, vUv.y + disp);
-  vec2 dispUv2 = vec2(vUv.x, vUv.y - disp);
-
-  vec4 texCurrent = texture(uTexCurrent, dispUv);
-  vec4 texNext = texture(uTexNext, dispUv2);
-
-  gl_FragColor = mix(texCurrent, texNext, uProgress);
-}
+        gl_FragColor = vec4(texture, 1.0);
+      }
